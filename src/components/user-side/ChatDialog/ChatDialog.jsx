ed3a426408f67dddec57bfe29991bfd39d9ca8e7
sneha <snehaksnehak470@gmail.com>
2024-10-17 11:50:43 +0530

    added the ip

--- src/components/user-side/ChatDialog/ChatDialog.jsx
+++ src/components/user-side/ChatDialog/ChatDialog.jsx
@@ -89,7 +89,7 @@ const ChatDrawer = ({ isOpen, onClose, currentUserId, receiverId, receiverName }
     if (isOpen && currentUserId && (receiverId || selectedPartner)) {
       const partnerIdToUse = receiverId || selectedPartner.id;
       const chatSocket = new WebSocket(
-        `ws://https://exploro.snehak.site/ws/chat/?receiver_id=${partnerIdToUse}&user_id=${currentUserId}`,
+        `wss://https://exploro.snehak.site/ws/chat/?receiver_id=${partnerIdToUse}&user_id=${currentUserId}`,
       );
 
       chatSocket.onopen = () => {
--- src/components/user-side/Notification/Notification.jsx
+++ src/components/user-side/Notification/Notification.jsx
@@ -32,7 +32,7 @@ const NotificationSystem = ({
 
   useEffect(() => {
     const socket = new WebSocket(
-      `ws://http://https://exploro.snehak.site/ws/notification/?user_id=${userId}`,
+      `wss://http://https://exploro.snehak.site/ws/notification/?user_id=${userId}`,
     );
 
     socket.onopen = () => {
